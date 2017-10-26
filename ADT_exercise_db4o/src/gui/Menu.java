27a1157e6c72a255b8d3489389a2f609c6821ad1
Jon Zaballa <jzaballazarzosa@gmail.com>
2017-10-26 09:56:31 +0200

    Third commit

--- ADT_exercise_db4o/src/gui/Menu.java
+++ ADT_exercise_db4o/src/gui/Menu.java
@@ -10,5 +10,14 @@ package gui;
  * @author ubuntu
  */
 public class Menu {
-    
+    public void display() {
+        System.out.println("--------------------------");
+        System.out.println("-----------MENU-----------");
+        System.out.println("--------------------------");
+        System.err.println("1) Create shopping list");
+        System.err.println("2) Create product");
+        System.err.println("3) Find product by category");
+        System.err.println("4) Add payer to shopping list");
+        System.err.println("5) Exit");
+    }
 }
